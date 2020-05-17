d065f2fd513c649bef035076a4c49c8c8ec1338d
cuiyang <cuiyangmail@163.com>
2020-05-17 17:58:33 +0800

    使用eslint格式化

--- src/views/usermanage/components/AddOrEditUser.vue
+++ src/views/usermanage/components/AddOrEditUser.vue
@@ -1,114 +1,115 @@
 <template>
-    <div id="user-dialog">
+  <div id="user-dialog">
     <el-dialog title="用户信息" :visible.sync="dialogVisible" width="40%">
-        <el-form :model="userDialogRow" size="mini">
-            <el-form-item label="用户名" :label-width="formLabelWidth">
-                <el-input v-model="userDialogRow.userName" autocomplete="off"></el-input>
-            </el-form-item>
-            <el-form-item label="性别" :label-width="formLabelWidth">
-                <el-select v-model.string="userDialogRow.userSex" placeholder="请选择性别">
-                    <el-option label="男" :value="1"></el-option>
-                    <el-option label="女" :value="0"></el-option>
-                </el-select>
-            </el-form-item>
-            、<el-form-item label="联系方式" :label-width="formLabelWidth">
-            <el-input v-model="userDialogRow.userPhone" autocomplete="off"></el-input>
+      <el-form :model="userDialogRow" size="mini">
+        <el-form-item label="用户名" :label-width="formLabelWidth">
+          <el-input v-model="userDialogRow.userName" autocomplete="off" />
         </el-form-item>
-            <el-form-item label="邮箱" :label-width="formLabelWidth">
-                <el-input v-model="userDialogRow.userEmail" autocomplete="off"></el-input>
-            </el-form-item>
-            <el-form-item label="出生日期" :label-width="formLabelWidth">
-                <el-date-picker
-                        v-model="userDialogRow.userBirthday"
-                        align="right"
-                        type="date"
-                        placeholder="选择日期"
-                        :picker-options="pickerOptions">
-                </el-date-picker>
-            </el-form-item>
-            <el-form-item label="是否删除" :label-width="formLabelWidth">
-                <el-switch
-                        v-model="userDialogRow.isDeleted"
-                        active-color="#13ce66"
-                        inactive-color="#ff4949"
-                        active-value="1"
-                        inactive-value="0">
-                </el-switch>
-            </el-form-item>
-            <el-form-item label="是否锁定" :label-width="formLabelWidth">
-                <el-switch
-                        v-model="userDialogRow.isLocked"
-                        active-color="#13ce66"
-                        inactive-color="#ff4949"
-                        active-value="1"
-                        inactive-value="0">
-                </el-switch>
-            </el-form-item>
-            <el-form-item label="创建时间" :label-width="formLabelWidth">
-                <el-input v-model="userDialogRow.createTime" readonly></el-input>
-            </el-form-item>
-        </el-form>
-        <div slot="footer" class="dialog-footer">
-            <el-button @click="changeDialogVisible()" size="mini">取 消</el-button>
-            <el-button type="primary" @click="dialogVisible = false" size="mini">确 定</el-button>
-        </div>
+        <el-form-item label="性别" :label-width="formLabelWidth">
+          <el-select v-model="userDialogRow.userSex" placeholder="请选择性别">
+            <el-option label="男" :value="1" />
+            <el-option label="女" :value="0" />
+          </el-select>
+        </el-form-item>
+        、
+        <el-form-item label="联系方式" :label-width="formLabelWidth">
+          <el-input v-model="userDialogRow.userPhone" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="邮箱" :label-width="formLabelWidth">
+          <el-input v-model="userDialogRow.userEmail" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="出生日期" :label-width="formLabelWidth">
+          <el-date-picker
+            v-model="userDialogRow.userBirthday"
+            align="right"
+            type="date"
+            placeholder="选择日期"
+            :picker-options="pickerOptions"
+          />
+        </el-form-item>
+        <el-form-item label="是否删除" :label-width="formLabelWidth">
+          <el-switch
+            v-model="userDialogRow.isDeleted"
+            active-color="#13ce66"
+            inactive-color="#ff4949"
+            active-value="1"
+            inactive-value="0"
+          />
+        </el-form-item>
+        <el-form-item label="是否锁定" :label-width="formLabelWidth">
+          <el-switch
+            v-model="userDialogRow.isLocked"
+            active-color="#13ce66"
+            inactive-color="#ff4949"
+            active-value="1"
+            inactive-value="0"
+          />
+        </el-form-item>
+        <el-form-item label="创建时间" :label-width="formLabelWidth">
+          <el-input v-model="userDialogRow.createTime" readonly />
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button size="mini" @click="changeDialogVisible()">取 消</el-button>
+        <el-button type="primary" size="mini" @click="dialogVisible = false">确 定</el-button>
+      </div>
     </el-dialog>
-    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "AddOrEditUser",
-        props: {
-            dialogVisible: {
-                type: Boolean,
-                required: false
-            },
-            userDialogRow:{
-                type:Object,
-                required: {}
-            }
-        },
-        data() {
-            return {
-
-                formLabelWidth: '120px',
-                pickerOptions: {
-                    disabledDate(time) {
-                        return time.getTime() > Date.now();
-                    },
-                    shortcuts: [{
-                        text: '今天',
-                        onClick(picker) {
-                            picker.$emit('pick', new Date());
-                        }
-                    }, {
-                        text: '昨天',
-                        onClick(picker) {
-                            const date = new Date();
-                            date.setTime(date.getTime() - 3600 * 1000 * 24);
-                            picker.$emit('pick', date);
-                        }
-                    }, {
-                        text: '一周前',
-                        onClick(picker) {
-                            const date = new Date();
-                            date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
-                            picker.$emit('pick', date);
-                        }
-                    }]
-                },
-                value1: '',
-                value2: '',
-
-            }
-        },
-        methods:{
-            changeDialogVisible(){
-                this.$emit("changeDialogVisible");
-            }
-        }
+export default {
+  name: 'AddOrEditUser',
+  props: {
+    dialogVisible: {
+      type: Boolean,
+      required: false
+    },
+    userDialogRow: {
+      type: Object,
+      required: {}
     }
+  },
+  data() {
+    return {
+
+      formLabelWidth: '120px',
+      pickerOptions: {
+        disabledDate(time) {
+          return time.getTime() > Date.now()
+        },
+        shortcuts: [{
+          text: '今天',
+          onClick(picker) {
+            picker.$emit('pick', new Date())
+          }
+        }, {
+          text: '昨天',
+          onClick(picker) {
+            const date = new Date()
+            date.setTime(date.getTime() - 3600 * 1000 * 24)
+            picker.$emit('pick', date)
+          }
+        }, {
+          text: '一周前',
+          onClick(picker) {
+            const date = new Date()
+            date.setTime(date.getTime() - 3600 * 1000 * 24 * 7)
+            picker.$emit('pick', date)
+          }
+        }]
+      },
+      value1: '',
+      value2: ''
+
+    }
+  },
+  methods: {
+    changeDialogVisible() {
+      this.$emit('changeDialogVisible')
+    }
+  }
+}
 </script>
 
 <style scoped>
--- src/views/usermanage/index.vue
+++ src/views/usermanage/index.vue
@@ -1,197 +1,205 @@
 <template>
-    <div class="user-block">
-        <el-card class="box-card" shadow="never">
-            <div>
-                <el-button size="small" type="primary" icon="el-icon-edit">添加用户</el-button>
-            </div>
+  <div class="user-block">
+    <el-card class="box-card" shadow="never">
+      <div>
+        <el-button size="small" type="primary" icon="el-icon-edit">添加用户</el-button>
+      </div>
 
-            <el-table
-                    ref="userTable"
-                    :data="tableData"
-                    size="mini"
-                    style="width: 100%;height: 100%"
-                    @selection-change="handleSelectionChange">
-                <el-table-column
-                        fixed
-                        type="selection"
-                        width="55">
-                </el-table-column>
-                <el-table-column
-                        fixed
-                        prop="userName"
-                        label="用户名"
-                        width="100">
-                </el-table-column>
-                <el-table-column
-                        fixed
-                        prop="userPhone"
-                        label="联系方式"
-                        width="100">
-                </el-table-column>
-                <el-table-column
-                        prop="userSex"
-                        label="性别"
-                        width="80"
-                        :formatter="formatBoolean">
-                </el-table-column>
-                <el-table-column
-                        prop="userEmail"
-                        label="邮箱"
-                        width="120">
-                </el-table-column>
-                <el-table-column
-                        prop="userBirthday"
-                        label="生日"
-                        width="100">
-                </el-table-column>
-                <el-table-column
-                        prop="isDeleted"
-                        label="是否删除"
-                        width="120"
-                >
-                    <template slot-scope="scope">
-                        <el-tag size="danger" v-if="scope.row.isDeleted">是</el-tag>
-                        <el-tag v-if="!scope.row.isDeleted">否</el-tag>
-                    </template>
-                </el-table-column>
-                <el-table-column
-                        prop="isLocked"
-                        label="是否锁定"
-                        width="120"
-                >
-                    <template slot-scope="scope">
-                        <el-tag size="danger" v-if="scope.row.isLocked">是</el-tag>
-                        <el-tag v-if="!scope.row.isLocked">否</el-tag>
-                    </template>
-                </el-table-column>
-                <el-table-column
-                        show-overflow-tooltip
-                        prop="userAvatar"
-                        label="用户头像"
-                        width="120">
-                </el-table-column>
-                <el-table-column
-                        show-overflow-tooltip
-                        prop="createTime"
-                        label="创建时间"
-                        width="150">
-                </el-table-column>
-                <el-table-column
-                        show-overflow-tooltip
-                        prop="updateTime"
-                        label="更新时间"
-                        width="150">
-                </el-table-column>
-                <el-table-column
-                        label="操作"
-                        width="150">
-                    <template slot-scope="scope">
-                        <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
-                        <el-button type="text" size="small">编辑</el-button>
-                        <el-tag type="danger" size="small">删除</el-tag>
-                    </template>
-                </el-table-column>
-            </el-table>
-            <div class="user-page">
-                <el-pagination
-                        background
-                        @size-change="handleSizeChange"
-                        @current-change="handleCurrentChange"
-                        :current-page="pageInfo.pageNum"
-                        :page-sizes="[10, 20, 30, 50,100]"
-                        :page-size="pageInfo.pageSize"
-                        layout="total, sizes, prev, pager, next, jumper"
-                        :total="pageInfo.size">
-                </el-pagination>
-            </div>
-        </el-card>
-        <div>
-            <add-or-edit-user v-if="userDialogVisible" :dialogVisible="userDialogVisible" :userDialogRow="userDialogRow" @changeDialogVisible="changeDialogVisible" id="add-or-edit-user"></add-or-edit-user>
-        </div>
+      <el-table
+        ref="userTable"
+        :data="tableData"
+        size="mini"
+        style="width: 100%;height: 100%"
+        @selection-change="handleSelectionChange"
+      >
+        <el-table-column
+          fixed
+          type="selection"
+          width="55"
+        />
+        <el-table-column
+          fixed
+          prop="userName"
+          label="用户名"
+          width="100"
+        />
+        <el-table-column
+          fixed
+          prop="userPhone"
+          label="联系方式"
+          width="100"
+        />
+        <el-table-column
+          prop="userSex"
+          label="性别"
+          width="80"
+          :formatter="formatBoolean"
+        />
+        <el-table-column
+          prop="userEmail"
+          label="邮箱"
+          width="120"
+        />
+        <el-table-column
+          prop="userBirthday"
+          label="生日"
+          width="100"
+        />
+        <el-table-column
+          prop="isDeleted"
+          label="是否删除"
+          width="120"
+        >
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.isDeleted" size="danger">是</el-tag>
+            <el-tag v-if="!scope.row.isDeleted">否</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="isLocked"
+          label="是否锁定"
+          width="120"
+        >
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.isLocked" size="danger">是</el-tag>
+            <el-tag v-if="!scope.row.isLocked">否</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column
+          show-overflow-tooltip
+          prop="userAvatar"
+          label="用户头像"
+          width="120"
+        />
+        <el-table-column
+          show-overflow-tooltip
+          prop="createTime"
+          label="创建时间"
+          width="150"
+        />
+        <el-table-column
+          show-overflow-tooltip
+          prop="updateTime"
+          label="更新时间"
+          width="150"
+        />
+        <el-table-column
+          label="操作"
+          width="150"
+        >
+          <template slot-scope="scope">
+            <el-button type="text" size="small" @click="handleClick(scope.row)">查看</el-button>
+            <el-button type="text" size="small">编辑</el-button>
+            <el-tag type="danger" size="small">删除</el-tag>
+          </template>
+        </el-table-column>
+      </el-table>
+      <div class="user-page">
+        <el-pagination
+          background
+          :current-page="pageInfo.pageNum"
+          :page-sizes="[10, 20, 30, 50,100]"
+          :page-size="pageInfo.pageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="pageInfo.size"
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+        />
+      </div>
+    </el-card>
+    <div>
+      <add-or-edit-user
+        v-if="userDialogVisible"
+        id="add-or-edit-user"
+        :dialog-visible="userDialogVisible"
+        :user-dialog-row="userDialogRow"
+        @changeDialogVisible="changeDialogVisible"
+      />
     </div>
+  </div>
 </template>
 
 <script>
 
-    import {getUsers} from "@/api/user/user";
-    import AddOrEditUser from "./components/AddOrEditUser";
-    export default {
-        name: "userManage",
-        components:{
-            AddOrEditUser
-        },
-        data() {
-            return {
-                pageInfo: {
-                    pageNum: 1,
-                    pageSize: 10,
-                    total: 0,
-                    isFirstPage: false,
-                    isLastPage: false,
-                    size: 0,
-                    pages: 1
-                },
-                tableData: [],
-                userDialogVisible:false,
-                userDialogRow:{}
-            }
+import { getUsers } from '@/api/user/user'
+import AddOrEditUser from './components/AddOrEditUser'
 
-        },
-
-        methods: {
-            getAllUser(pageNum, pageSize) {
-                getUsers(pageNum, pageSize).then(res => {
-                    this.tableData = res.data.data.pageInfo.list;
-                    this.pageInfo=res.data.data.pageInfo;
-                }).catch(error => {
-
-                })
-            },
-            formatBoolean(row, column, cellValue) {
-
-                var ret = ''  //你想在页面展示的值
-                if (column.property == "userSex") {
-                    if (cellValue) {
-                        ret = "男"  //根据自己的需求设定
-                    } else {
-                        ret = "女"
-                    }
-                }
-                return ret;
-            },
-            handleClick(row) {
-                console.log(row);
-                this.userDialogVisible=!this.userDialogVisible;
-                this.userDialogRow=Object.assign({},row);
-            },
-            changeDialogVisible(){
-                this.userDialogVisible=!this.userDialogVisible;
-            },
-            handleSizeChange(val) {
-                console.log(`每页 ${val} 条`);
-                this.getAllUser(this.pageInfo.pageNum, val);
-            },
-            handleCurrentChange(val) {
-                console.log(`当前页: ${val}`);
-                this.getAllUser(val, this.pageInfo.pageSize);
-            },
-            toggleSelection(rows) {
-                if (rows) {
-                    rows.forEach(row => {
-                        this.$refs.userTable.toggleRowSelection(row);
-                    });
-                } else {
-                    this.$refs.userTable.clearSelection();
-                }
-            },
-            handleSelectionChange(val) {
-                this.multipleSelection = val;
-            }
-        },
-        mounted() {
-            this.getAllUser(1, 10);
-        }
+export default {
+  name: 'UserManage',
+  components: {
+    AddOrEditUser
+  },
+  data() {
+    return {
+      pageInfo: {
+        pageNum: 1,
+        pageSize: 10,
+        total: 0,
+        isFirstPage: false,
+        isLastPage: false,
+        size: 0,
+        pages: 1
+      },
+      tableData: [],
+      userDialogVisible: false,
+      userDialogRow: {}
     }
+  },
+  mounted() {
+    this.getAllUser(1, 10)
+  },
+
+  methods: {
+    getAllUser(pageNum, pageSize) {
+      getUsers(pageNum, pageSize).then(res => {
+        this.tableData = res.data.data.pageInfo.list
+        this.pageInfo = res.data.data.pageInfo
+        // eslint-disable-next-line handle-callback-err
+      }).catch(error => {
+
+      })
+    },
+    formatBoolean(row, column, cellValue) {
+      var ret = '' // 你想在页面展示的值
+      if (column.property === 'userSex') {
+        if (cellValue) {
+          ret = '男' // 根据自己的需求设定
+        } else {
+          ret = '女'
+        }
+      }
+      return ret
+    },
+    handleClick(row) {
+      console.log(row)
+      this.userDialogVisible = !this.userDialogVisible
+      this.userDialogRow = Object.assign({}, row)
+    },
+    changeDialogVisible() {
+      this.userDialogVisible = !this.userDialogVisible
+    },
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`)
+      this.getAllUser(this.pageInfo.pageNum, val)
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`)
+      this.getAllUser(val, this.pageInfo.pageSize)
+    },
+    toggleSelection(rows) {
+      if (rows) {
+        rows.forEach(row => {
+          this.$refs.userTable.toggleRowSelection(row)
+        })
+      } else {
+        this.$refs.userTable.clearSelection()
+      }
+    },
+    handleSelectionChange(val) {
+      this.multipleSelection = val
+    }
+  }
+}
 </script>
 
 <style scoped>
